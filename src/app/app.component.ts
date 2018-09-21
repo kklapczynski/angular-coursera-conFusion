ef1467ff384db9ddb9f3a8a0f2c81362c25862ea
Krzysztof Klapczyński <k.klapczynski@gmail.com>
2018-09-21 15:05:44 +0200

    App configuration for using Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'conFusion';
+  title = 'conFusion - the APP';
   // comment for testing git
 }
