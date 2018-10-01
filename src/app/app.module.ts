700b147acc39558450871b98a827716e513e910b
Krzysztof Klapczyński <k.klapczynski@gmail.com>
2018-10-01 13:00:36 +0200

    SPA part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,7 @@ import { DishService } from './services/dish.service';
     MatListModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -10,4 +10,10 @@ export class DishService {
   getDishes(): Dish[] {
     return DISHES;
   }
+  getDish(id: number) {
+      return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+  getFeaturedDish() {
+      return DISHES.filter((dish) => (dish.featured))[0];
+  }
 }
