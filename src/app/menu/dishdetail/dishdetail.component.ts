5929f8188b137ad17993dd7afa2755c4b2299fdf
Krzysztof Klapczyński <k.klapczynski@gmail.com>
2018-10-02 15:19:37 +0200

    SPA part 2

--- src/app/menu/dishdetail/dishdetail.component.ts
+++ src/app/menu/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DishService } from '../../services/dish.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../../shared/dish';
+
 
 @Component({
   selector: 'app-dishdetail',
@@ -10,11 +14,16 @@ import { Dish } from '../../shared/dish';
     // 1.Add event binding and handler in menu component: click on mat-grid-tile = dish
     // 2.Pass selected dish to dishdetail component
 export class DishdetailComponent implements OnInit {
-    @Input()
+    //@Input() changing to use routerLink
     dish: Dish;
-    constructor() { }
+    constructor(private route: ActivatedRoute, private dishService: DishService, private location: Location) { }
 
     ngOnInit() {
+        const id = +this.route.snapshot.params['id'];
+        this.dish = this.dishService.getDish(id);
+    }
+    goBack(): void {
+        this.location.back();
     }
 
 }
